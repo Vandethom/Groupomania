5fcc0117046e5f2f6939888ac3ee42874718ac07
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2020-09-17 11:29:08 +0200

    first commit, intialized vue

---
+++ frontend/src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="navbar">
+      <nav>
+        <a href="#">Logo</a>
+        <ul>
+          <li>Une searchbar</li>
+          <li>Mon compte</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+
+<style>
+</style>
