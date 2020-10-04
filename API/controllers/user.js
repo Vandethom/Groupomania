19e95fea87320f3f6d482868adfcfd48ef1238aa
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2020-10-04 15:30:09 +0200

    signup route almost ready

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -5,17 +5,17 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env.MySQLPassword,
     database: "groupomania"
 });
 const Sequelize = require('sequelize');
-const connection = new Sequelize('groupomania', 'root', process.env.MySQLPassword, {
+/*const connection = new Sequelize('groupomania', 'root', process.env.MySQLPassword, {
     host: 'localhost',
     dialect: 'mysql'
-}); //connecting with 3 arguments : db_name/user/password
+}); //connecting with 3 arguments : db_name/user/password*/
 
 const models = require('../models/user');
 
@@ -52,6 +52,23 @@ function maskEmail(email) {
 /* ------------------------- Controllers ------------------------- */
 
 /* ------------------------- Signing Up User ------------------------- */
+
+exports.signup = (req, res, next) => {
+    connection.connect(function (error) {
+        if (error) throw err;
+        const values = `('Martha Jane', 'Canary', 'Calamity Jane', '01081852', 'F', 'calmm@msn.fr', 'I am a f***ing gangster !')`;
+        const sql = `INSERT INTO Users (name, surname, pseudonym, password, gender, email, description) 
+        VALUES ${values}`;
+
+        connection.query(sql, function (error, result) {
+            if (error) throw error;
+            console.log('L\'utilisateur vient d\être ajouté aux profils');
+        })
+    })
+}
+
+
+/* Celle qui fonctionne.... presque;
 exports.signup = (req, res, next) => {
     connection.sync()
         .then(function () {
@@ -71,7 +88,7 @@ exports.signup = (req, res, next) => {
         }))
         .catch(error => res.status(500).json({ error: 'Try again !' }))
 };
-/*
+
 connection.sync().then(function () {
     User.create({
         name: 'Jake',
@@ -138,11 +155,13 @@ exports.login = (req, res, next) => {
 /* ------------------------- Retrieving Account Informations ------------------------- */
 
 exports.getUser = (req, res, next) => {
-    con.connect(function (err) {
-        if (err) throw err;
-        con.query("SELECT * FROM Users", function (err, result, fields) {
-            if (err) throw err;
+    connection.connect(function (error) {
+        if (error) throw err;
+        connection.query("SELECT * FROM Users", function (error, result, fields) {
+            if (error) throw error;
             console.log(result);
         });
-    });
+    })
+        .then((result) => res.status(200).json({ result }))
+        .catch((error) => res.status(500).json({ error: 'Bah ça marche pas.' }))
 };
